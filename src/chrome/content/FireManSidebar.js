88266b7b4c0b5f49fa6a8c547d13874d06f4a080
Tobias Sarnowski <sarnowski@new-thoughts.org>
2008-07-18 18:42:49 +0200

    Logo and ToolbarButton added A new logo is added and a toolbar button is introduced in the overlay. There are two known bugs which have to be fixed by time. One is an ugly not-resize bug where the bug icon is shown even if the button is in a small toolbar and the other one is a javascript/firefox problem with a mysteric source.

--- src/chrome/content/FireManSidebar.js
+++ src/chrome/content/FireManSidebar.js
@@ -1,4 +1,14 @@
 function FM_fillTree(searchTerm)
 {
-	var data = FM_getList(searchTerm);
+	try {
+		var tree = window.document.getElementById("ManualPageTree");
+		var data = FM_getList(searchTerm);
+
+		// clear the tree
+
+		// add tree
+
+	} catch (ex) {
+		alert("FireMan Exception:\n" + ex);
+	}
 }
