413fba09e5bcf881b8ce76ce5ce853464a15c62e
Tobias Sarnowski <sarnowski@cosmocode.de>
2008-08-29 18:58:45 +0200

    basic man page parsing to html firefox does not recognize the html correctly and doesn't load the css.

--- src/components/ManViewer.js
+++ src/components/ManViewer.js
@@ -18,6 +18,112 @@ const nsIPrefService        		= Components.interfaces.nsIPrefService;
 const nsIWindowWatcher      		= Components.interfaces.nsIWindowWatcher;
 const nsIChannel            		= Components.interfaces.nsIChannel;
 
+function transformMan2HTML(manoutput)
+{
+	var htmloutput = '';
+
+	var on_bold = false;
+	var on_underline = false;
+	for (var n = 0; n < manoutput.length; n++) {
+		var s = manoutput.substr(n, 1);
+		var c = s.charCodeAt(0);
+
+		var s_next = '';
+		var c_next = 0;
+		if (n < manoutput.length - 1) {
+			s_next = manoutput.substr(n + 1, 1);
+			c_next = s_next.charCodeAt(0);
+		}
+
+		var s_nextnext = '';
+		var c_nextnext = 0;
+		if (n < manoutput.length - 2) {
+			s_nextnext = manoutput.substr(n + 2, 1);
+			c_nextnext = s_nextnext.charCodeAt(0);
+		}
+
+		var s_nextnextnext = '';
+		var c_nextnextnext = 0;
+		if (n < manoutput.length - 3) {
+			s_nextnextnext = manoutput.substr(n + 3, 1);
+			c_nextnextnext = s_nextnextnext.charCodeAt(0);
+		}
+
+		var s_prev = '';
+		var c_prev = 0;
+		if (n > 0) {
+			s_prev = manoutput.substr(n - 1, 1);
+			c_prev = s_prev.charCodeAt(0);
+		}
+
+		// start parsing
+
+		if (s == '\n') {
+			// new line, close the old one
+			// clear bold/underline
+			if (on_bold || on_underline) {
+				htmloutput += '</span>';
+				on_bold = false;
+				on_underline = false;
+			}
+			htmloutput += "<br/>\n";
+
+		} else if (s == '<') {
+			htmloutput += '&lt;';
+		} else if (s == '>') {
+			htmloutput += '&gt;';
+		} else if (s == '&') {
+			htmloutput += '&and;';
+		} else if (s == '+' && s_next != '+' && on_bold) {
+			htmloutput += '*'; // it's a bullet
+
+		} else if (c == 8) {
+			// should be already parsed
+			continue;
+
+		} else if (s == '_' && c_next == 8) {
+			// underline
+			if (!on_underline) {
+				htmloutput += '<span class="man_underline">';
+				on_underline = true;
+			}
+			htmloutput += s_nextnext;
+			n += 2;
+		} else if (c_next == 8 && s_nextnext == s) {
+			// bold
+			if (!on_bold) {
+				htmloutput += '<span class="man_bold">';
+				on_bold = true;
+			}
+			htmloutput += s;
+			n += 2;
+		} else {
+			if (on_bold || on_underline) {
+				htmloutput += '</span>';
+				on_bold = false;
+				on_underline = false;
+			}
+			htmloutput += s;
+		}
+
+	}
+
+	var tmp = htmloutput;
+
+	htmloutput  = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"\n';
+	htmloutput += '    "http://www.w3.org/TR/html4/loose.dtd">\n\n';
+	htmloutput += '<html>\n';
+	htmloutput += '    <head>\n';
+	htmloutput += '        <title>FireMan</title>\n';
+	htmloutput += '        <link rel="stylesheet" type="text/css" href="chrome://fireman/skin/basic.css"/>\n';
+	htmloutput += '    </head>\n';
+	htmloutput += '    <body>\n';
+	htmloutput += tmp;
+	htmloutput += '    </body>\n';
+	htmloutput += '</html>\n\n';
+
+	return htmloutput;
+}
 
 /***** ProtocolHandler *****/
 
@@ -72,8 +178,21 @@ ManProtocolHandler.prototype.newChannel = function(aURI)
 
 
 	// now get the man output and parse it
-	// TODO ...
-	var output = '<html><head><title>' + title + ' (' + category + ')</title></head><body><h1>' + title + ' (' + category + ')</h1></body></html>';
+
+	// get out components
+	var shell = Components.classes['%%XPCOM_SHELL%%'].getService().wrappedJSObject;
+
+	var mancmd = 'man';
+	if (category) {
+		mancmd = mancmd + ' -s ' + category;
+	}
+	if (machine) {
+		mancmd = mancmd + ' -S ' + machine;
+	}
+	mancmd = mancmd + ' ' + title;
+	var output = shell.exec(mancmd);
+
+	output = transformMan2HTML(output);
 
 	var stream = Components.classes["@mozilla.org/io/string-input-stream;1"].createInstance(Components.interfaces.nsIStringInputStream);
 	stream.setData(output, output.length);
